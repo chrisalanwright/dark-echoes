e82ce5987d43843c3e3b346f4924482b76cd44bb
Christopher Wright <chrisalanwright@gmail.com>
2025-08-28 22:12:46 -0400

    started-workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+}
+
+function EpisodeDetails() {
+  if (!selectedEpisode) {
+    return (
+      <section className="details">
+        <h2>Episode Details</h2>
+        <p>Select an episode to learn more.</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className="details">
+      <h2>{selectedEpisode.title}</h2>
+      <p>
+        {selectedEpisode.title} is episode number {selectedEpisode.id}
+      </p>
+      <p>{selectedEpisode.description}</p>
+    </section>
+  );
+}
+
+function Episodes() {
+  return (
+    <section className="episodes">
+      <h2>Episodes</h2>
+    </section>
+  );
 }
